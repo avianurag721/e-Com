97641b16d0c9befbf37b836e976f858b79c89bcd
avianurag721 <anurag.721721@gmail.com>
2023-02-16 16:49:27 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter,  Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
